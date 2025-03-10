6d896d8c7483f69d141ba880134ecbf6114c1463
Azmat2575 <84296155+Azmat2575@users.noreply.github.com>
2025-03-10 21:44:40 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function recharge() {
+    let phone = document.getElementById("phoneNumber").value;
+    let amount = document.getElementById("amount").value;
+    alert(phone + " پر " + amount + " روپے کا ریچارج ہو رہا ہے!");
+}
